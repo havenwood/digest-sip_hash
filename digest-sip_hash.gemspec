248ac2c74913e7e35d2d5b5f8744eb6eabff1180
Shannon Skipper <shannonskipper@gmail.com>
2018-06-27 23:06:45 -0700

    Add minitest hell and proveit

--- digest-sip_hash.gemspec
+++ digest-sip_hash.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'rake', '~> 12'
   spec.add_development_dependency 'minitest', '~> 5'
+  spec.add_development_dependency 'minitest-proveit'
 end
--- spec/helper.rb
+++ spec/helper.rb
@@ -5,4 +5,9 @@ $LOAD_PATH.prepend lib unless $LOAD_PATH.include? lib
 
 require 'digest/sip_hash'
 require 'minitest/autorun'
+require 'minitest/hell'
 require 'minitest/pride'
+
+class Minitest::Test
+  prove_it!
+end
